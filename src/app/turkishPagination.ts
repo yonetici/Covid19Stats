b07c26809c3e83d3d4eeebf72a814f4f649adfbb
yonetici <segate@gmail.com>
2020-03-26 15:39:17 +0300

    Revert "Finished 7/8"

---
+++ src/app/turkishPagination.ts
@@ -0,0 +1,28 @@
+import { MatPaginatorIntl } from '@angular/material';
+
+const TurkishRangeLabel = (page: number, pageSize: number, length: number) => {
+  if (length == 0 || pageSize == 0) { return `0 van ${length}`; }
+
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize;
+
+  // If the start index exceeds the list length, do not try and fix the end index to the end.
+  const endIndex = startIndex < length ?
+    Math.min(startIndex + pageSize, length) :
+    startIndex + pageSize;
+
+  return `${startIndex + 1} - ${endIndex} / ${length}`;
+}
+
+
+export function getTurkishPaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+
+  paginatorIntl.itemsPerPageLabel = 'Sayfa Başına:';
+  paginatorIntl.nextPageLabel = 'Sonraki Sayfa';
+  paginatorIntl.previousPageLabel = 'Önceki Sayfa';
+  paginatorIntl.getRangeLabel = TurkishRangeLabel;
+
+  return paginatorIntl;
+}
--- src/app/ulkeler/ulkeler.component.ts
+++ src/app/ulkeler/ulkeler.component.ts
@@ -3,7 +3,6 @@ import {MatTableDataSource} from '@angular/material/table';
 import {ApiService} from '../api.service';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
-import {MatInput} from '@angular/material/input';
 import {ngxCsv} from 'ngx-csv';
 
 export interface PeriodicElement {
@@ -46,6 +45,7 @@ export class UlkelerComponent implements OnInit {
     decimalseparator: '.',
     showLabels: true,
     useBom: true,
+    // tslint:disable-next-line:max-line-length
     headers: ['country', 'cases', 'todayCases', 'deaths', 'todayDeaths', 'recovered', 'active', 'critical', 'casesPerOneMillion', 'deathsPerOneMillion']
   };
 
@@ -56,7 +56,26 @@ export class UlkelerComponent implements OnInit {
       this.dataSource = new MatTableDataSource<PeriodicElement>(this.datas);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+/*      this.paginator._intl.itemsPerPageLabel = 'Sayfa Başına Gösterim';
+      this.paginator._intl.nextPageLabel  = 'Sonraki Sayfa';
+      this.paginator._intl.previousPageLabel = 'Önceki Sayfa';
+      // @ts-ignore
+      // tslint:disable-next-line:only-arrow-functions
+      this.paginator._intl.getRangeLabel = function(page, pageSize, length) {
+        if (length === 0 || pageSize === 0) {
+          return '0 od ' + length;
+        }
+        length = Math.max(length, 0);
+        const startIndex = page * pageSize;
+        // If the start index exceeds the list length, do not try and fix the end index to the end.
+        const endIndex = startIndex < length ?
+          Math.min(startIndex + pageSize, length) :
+          startIndex + pageSize;
+        return startIndex + 1 + ' - ' + endIndex + ' / ' + length;
+      };
+*/
     });
+
   }
 
   applyFilter(event: Event) {
